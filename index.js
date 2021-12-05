8a059b55efd40a6634fe918d1499d8855d172801
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-12-04 23:52:37 -0500

    Added files

---
+++ index.js
@@ -0,0 +1,34 @@
+// const { fetchMyIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+//console.log('------------   Spotter II ------------\n');
+// index.js
+
+// The code below is temporary and can be commented out.
+//
+//==================== 3 ================
+
+// index.js
+
+
+// The code below is temporary and can be commented out.
+const { fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
